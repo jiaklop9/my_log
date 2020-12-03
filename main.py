423d7e885158da53294b49479f6146a5e3e8aa71
liuzhike <1714476383@qq.com>
2020-12-03 09:34:22 +0800

    封装

--- main.py
+++ main.py
@@ -3,21 +3,43 @@
 
 import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(level=logging.WARNING)
-sh = logging.StreamHandler()
 
-fmt = "%(asctime)-15s %(levelname)s %(filename)s %(lineno)d %(message)s"
-formatter = logging.Formatter(fmt)
-sh.setFormatter(formatter)
-logger.addHandler(sh)
+class Logger:
+    def __init__(self, path, console_level=logging.DEBUG, file_level=logging.INFO):
+        self.logger = logging.getLogger(path)
+        self.logger.setLevel(logging.DEBUG)
+        fmt = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s', '%Y-%m-%d %H:%M:%S')
+        # 设置CMD日志
+        sh = logging.StreamHandler()
+        sh.setFormatter(fmt)
+        sh.setLevel(console_level)
+        # 设置文件日志
+        fh = logging.FileHandler(path)
+        fh.setFormatter(fmt)
+        fh.setLevel(file_level)
+        self.logger.addHandler(sh)
+        self.logger.addHandler(fh)
 
-logger.debug("debug")
-logger.info("info")
-logger.warning("warn")
-logger.error("error")
-logger.critical("critical")
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
 
 
 if __name__ == '__main__':
-    pass
+    logyyx = Logger('yyx.log', logging.ERROR, logging.WARNING)
+    logyyx.debug('一个debug信息')
+    logyyx.info('一个info信息')
+    logyyx.war('一个warning信息')
+    logyyx.error('一个error信息')
+    logyyx.cri('一个致命critical信息')
